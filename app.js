9d3665b79796fa03343c042e6fc989ec4dfde87b
Atifullah <63223196+Atifullah@users.noreply.github.com>
2025-05-21 17:25:48 +0500

    middlewear

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const fs = require('fs');
 
-const app = express();
-
 // Start the server
 const PORT = 3000;
-
 // Middleware
+
+const app = express();
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
 app.use(express.json());
+app.use((req, res, next) => {
+  req.todaydate = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// Routes Handlers
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
+    requestTime: req.todaydate,
     data: {
       tours,
     },
@@ -99,13 +108,8 @@ const createTour = (req, res) => {
     }
   );
 };
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
 
-// app.patch('/api/v1/tours/:id', updateTour);
-
-// app.delete('/api/v1/tours/:id', deleteTour);
-// app.post('/api/v1/tours', createTour);
+// Routes
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
@@ -113,6 +117,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// server start
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
