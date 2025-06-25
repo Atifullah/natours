51f9014097c75bb1a9c8bbd815ee05b04446bd0e
Atifullah <63223196+Atifullah@users.noreply.github.com>
2025-06-25 12:24:46 +0500

    connecting databse mongodb

--- server.js
+++ server.js
@@ -1,10 +1,64 @@
-// server start
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+
 dotenv.config({ path: './config.env' });
+
+// Replace <PASSWORD> in DB string
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// MongoDB connection
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour Must have Price'],
+  },
+});
+
+const Tour = mongoose.model('tour', tourSchema);
+
+const tourTest = new Tour({
+  name: 'the park camper',
+  price: 400,
+});
+tourTest
+  .save()
+  .then((sav) => {
+    console.log('saved', sav);
+  })
+  .catch((err) => {
+    console.log('not saved error', err);
+  });
+
 // Start the server
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
